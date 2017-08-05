7445cbe09a6c713f764bcf41b96c874d6a933358
ChanJoo <sksckswn@naver.com>
2017-08-06 02:07:00 +0900

    intro activity 구현

---
+++ app/src/main/java/com/tistory/dayglo/jibcon_re/IntroActivity.java
@@ -0,0 +1,34 @@
+package com.tistory.dayglo.jibcon_re;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by user on 2017-08-06.
+ */
+
+public class IntroActivity extends AppCompatActivity {
+    private Handler mHandler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.intro_activity);
+
+        mHandler = new Handler();
+        mHandler.postDelayed(mRunnable, 1500);
+    }
+
+    private Runnable mRunnable = new Runnable() {
+        @Override
+        public void run() {
+            Intent intent = new Intent(getApplicationContext(), TutorialActivity.class);
+            startActivity(intent);
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+        }
+    };
+
+
+}
---
+++ app/src/main/java/com/tistory/dayglo/jibcon_re/TutorialActivity.java
@@ -0,0 +1,13 @@
+package com.tistory.dayglo.jibcon_re;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TutorialActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tutorial_activity);
+    }
+}
